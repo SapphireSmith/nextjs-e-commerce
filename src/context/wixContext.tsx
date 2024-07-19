9e14aaab7e2b76f16e2fa89334eda4f055565f4a
Sapphire Smith <ssapphire527@gmail.com>
2024-07-19 22:10:35 +0530

    wix context added

---
+++ src/context/wixContext.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { products, collections } from "@wix/stores"
+import Cookies from 'js-cookie'
+import { createContext, ReactNode } from "react";
+
+const clientId: any = process.env.NEXT_PUBLIC_WIX_CLIENT_ID;
+
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}")
+
+const wixClient = createClient({
+    modules: {
+        products,
+        collections
+        //   services
+    },
+    auth: OAuthStrategy({
+        clientId,
+        tokens: {
+            refreshToken, accessToken: { value: "", expiresAt: 0 }
+        }
+    })
+})
+
+export type WixClient = typeof wixClient
+
+export const WixClientContext = createContext<WixClient>(wixClient)
+
+export const WixClientContextProvider = ({ children }: { children: ReactNode }) => {
+    return (
+        <WixClientContext.Provider value={wixClient}>{children}</WixClientContext.Provider>
+    )
+}
